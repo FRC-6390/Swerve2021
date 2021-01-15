a5728be28198e0ba480d31499c286ea69a777abb
mohammadawwad <mohd-awwad@hotmail.com>
2021-01-14 22:24:12 -0800

    declared motors in drivetrain subsystem and gyro. declared gyros the way I reaad its supposed to be done but it doesnt seem right

--- Swerve2021/src/main/java/frc/robot/Constants.java
+++ Swerve2021/src/main/java/frc/robot/Constants.java
@@ -1,5 +1,7 @@
 package frc.robot;
 
+import com.ctre.phoenix.motorcontrol.TalonSRXFeedbackDevice;
+
 public final class Constants {
 
     public static final int JOYSTICK_CONTROLLER = 0;
@@ -7,9 +9,24 @@ public final class Constants {
 
     /*-------------------------MOTORS--------------------*/
     // DRIVE TRAIN MOTORS
-    public static final int FRONT_RIGHT_MOTOR_ID = 4; // Spark
-    public static final int BACK_RIGHT_MOTOR_ID = 3; // Spark
-    public static final int FRONT_LEFT_MOTOR_ID = 2; // Spark
-    public static final int BACK_LEFT_MOTOR_ID = 1; // Spark
-    // HOPPER MOTORS //
+    public static final int FRONT_LEFT_MOTOR_1_ID = 0;
+    public static final int FRONT_LEFT_MOTOR_2_ID = 0;
+    public static final int BACK_LEFT_MOTOR_1_ID = 0;
+    public static final int BACK_LEFT_MOTOR_2_ID = 0;
+    public static final int FRONT_RIGHT_MOTOR_1_ID = 0;
+    public static final int FRONT_RIGHT_MOTOR_2_ID = 0;
+    public static final int BACK_RIGHT_MOTOR_1_ID = 0;
+    public static final int BACK_RIGHT_MOTOR_2_ID = 0;
+    // DRIVE TRAIN ENCODERS
+    public static final TalonSRXFeedbackDevice FRONT_LEFT_MOTOR_ENCODER_1 = null;
+    public static final TalonSRXFeedbackDevice FRONT_LEFT_MOTOR_ENCODER_2 = null;
+    public static final TalonSRXFeedbackDevice BACK_LEFT_MOTOR_ENCODER_1 = null;
+    public static final TalonSRXFeedbackDevice BACK_LEFT_MOTOR_ENCODER_2 = null;
+    public static final TalonSRXFeedbackDevice FRONT_RIGHT_MOTOR_ENCODER_1 = null;
+    public static final TalonSRXFeedbackDevice FRONT_RIGHT_MOTOR_ENCODER_2 = null;
+    public static final TalonSRXFeedbackDevice BACK_RIGHT_MOTOR_ENCODER_1 = null;
+    public static final TalonSRXFeedbackDevice BACK_RIGHT_MOTOR_ENCODER_2 = null;
+
+    public static final int PDP_DEVICE_ID = 0;
+
 }
--- Swerve2021/src/main/java/frc/robot/subsystems/DriveTrain.java
+++ Swerve2021/src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -1,33 +1,64 @@
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
-
-import com.ctre.phoenix.motorcontrol.can.TalonFX;
-import com.revrobotics.CANEncoder;
-import com.revrobotics.CANSparkMax;
-import com.revrobotics.CANSparkMaxLowLevel.MotorType;
-
-import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.PowerDistributionPanel;
-import edu.wpi.first.wpilibj.SerialPort.Port;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.I2C.Port;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.kauailabs.navx.frc.AHRS;
+
 import frc.robot.Constants;
 
 public class DriveTrain extends SubsystemBase {
-  public DriveTrain() {
-    final TalonFX frontLeftMotor = new TalonFX(Constants.FRONT_LEFT_MOTOR_ID);
-    final TalonFX backLeftMotor = new TalonFX(Constants.BACK_LEFT_MOTOR_ID);
-    final TalonFX frontRightMotor = new TalonFX(Constants.FRONT_RIGHT_MOTOR_ID);
-    final TalonFX backRightMotor = new TalonFX(Constants.BACK_RIGHT_MOTOR_ID);
 
-    final CANEncoder frontLeftEncoder = new CANSparkMax(Constants.FRONT_LEFT_MOTOR_ID, MotorType.kBrushless)
-        .getEncoder();
-    final CANEncoder backLeftEncoder = new CANSparkMax(Constants.BACK_LEFT_MOTOR_ID, MotorType.kBrushless).getEncoder();
-    final CANEncoder frontRightEncoder = new CANSparkMax(Constants.FRONT_RIGHT_MOTOR_ID, MotorType.kBrushless)
-        .getEncoder();
-    final CANEncoder backRightEncoder = new CANSparkMax(Constants.BACK_RIGHT_MOTOR_ID, MotorType.kBrushless)
-        .getEncoder();
+  private TalonSRX frontLeftMotor_1 = new TalonSRX(Constants.FRONT_LEFT_MOTOR_1_ID);
+  private TalonSRX frontLeftMotor_2 = new TalonSRX(Constants.FRONT_LEFT_MOTOR_2_ID);
+  private TalonSRX backLeftMotor_1 = new TalonSRX(Constants.BACK_LEFT_MOTOR_1_ID);
+  private TalonSRX backLeftMotor_2 = new TalonSRX(Constants.BACK_LEFT_MOTOR_2_ID);
+  private TalonSRX frontRightMotor_1 = new TalonSRX(Constants.FRONT_RIGHT_MOTOR_1_ID);
+  private TalonSRX frontRightMotor_2 = new TalonSRX(Constants.FRONT_RIGHT_MOTOR_2_ID);
+  private TalonSRX backRightMotor_1 = new TalonSRX(Constants.BACK_RIGHT_MOTOR_1_ID);
+  private TalonSRX backRightMotor_2 = new TalonSRX(Constants.BACK_RIGHT_MOTOR_2_ID);
+
+  /*
+   * frontLeftMotor_1.configSelectedFeedbackSensor(Constants.
+   * FRONT_LEFT_MOTOR_ENCODER_1, 0, 10);
+   * frontLeftMotor_2.configSelectedFeedbackSensor(Constants.
+   * FRONT_LEFT_MOTOR_ENCODER_2, 0, 10);
+   * backLeftMotor_1.configSelectedFeedbackSensor(Constants.
+   * BACK_LEFT_MOTOR_ENCODER_1, 0, 10);
+   * backLeftMotor_2.configSelectedFeedbackSensor(Constants.
+   * BACK_LEFT_MOTOR_ENCODER_2, 0, 10);
+   * frontRightMotor_1.configSelectedFeedbackSensor(Constants.
+   * FRONT_RIGHT_MOTOR_ENCODER_1, 0, 10);
+   * frontRightMotor_2.configSelectedFeedbackSensor(Constants.
+   * FRONT_RIGHT_MOTOR_ENCODER_2, 0, 10);
+   * backRightMotor_1.configSelectedFeedbackSensor(Constants.
+   * BACK_RIGHT_MOTOR_ENCODER_1, 0, 10);
+   * backRightMotor_2.configSelectedFeedbackSensor(Constants.
+   * BACK_RIGHT_MOTOR_ENCODER_2, 0, 10);
+   */
+
+  /*
+   * 
+   * Front Front Left Right |-----------------------------| | 1 | \Intake/ | 1 | |
+   * 2 | | 2 | |-----| |-----| | | | | | |Revolover| | | | | | |-----| |-----| | 2
+   * | | 2 | | 1 | /Shooter\ | 1 | |-----------------------------| Bottom Bottom
+   * Left Right
+   * 
+   */
+
+  // GYRO
+  private AHRS gyro = new AHRS(Port.kMXP);
+  // PDP
+  final PowerDistributionPanel PDP = new PowerDistributionPanel(Constants.PDP_DEVICE_ID);
+
+  public void resetGyro() {
+    gyro.reset();
+    gyro.zeroYaw();
+  }
+
+  public DriveTrain() {
 
   }
 
