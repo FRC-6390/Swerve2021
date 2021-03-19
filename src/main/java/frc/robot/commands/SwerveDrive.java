a2fbae767e040ab6a9cdca31b8ada6dcee40581a
mohammadawwad <mohd-awwad@hotmail.com>
2021-03-19 15:31:00 -0700

    updating

--- src/main/java/frc/robot/commands/SwerveDrive.java
+++ src/main/java/frc/robot/commands/SwerveDrive.java
@@ -14,9 +14,9 @@ public class SwerveDrive extends CommandBase {
   private XboxController controller;
 
   //Slew Limmiter smooths out controller inputs
-  private final SlewRateLimiter xspeedLimiter = new SlewRateLimiter(6);
-  private final SlewRateLimiter yspeedLimiter = new SlewRateLimiter(6);
-  private final SlewRateLimiter rotLimiter = new SlewRateLimiter(6);
+  private final SlewRateLimiter xspeedLimiter = new SlewRateLimiter(3);
+  private final SlewRateLimiter yspeedLimiter = new SlewRateLimiter(3);
+  private final SlewRateLimiter rotLimiter = new SlewRateLimiter(3);
   
   public SwerveDrive(SwerveDriveTrain driveTrain, XboxController controller) {
     this.driveTrain = driveTrain;
@@ -30,38 +30,14 @@ public class SwerveDrive extends CommandBase {
   @Override
   public void execute() {
 
-    //Tennery used to control the deadzone     if less than max and more than -min then 0 else move
-    //Regular Movement                            {^ deadzone                     }    
-    // double leftY = controller.getY(GenericHID.Hand.kLeft) <= Constants.ROBOT.DEAD_ZONE_MAX.get() && controller.getY(GenericHID.Hand.kLeft) >= -Constants.ROBOT.DEAD_ZONE_MIN.get() ? 0.0 : -xspeedLimiter.calculate(controller.getY(GenericHID.Hand.kLeft)) * Constants.ROBOT.MAX_SPEED.get();
-    // double leftX = controller.getX(GenericHID.Hand.kLeft) <= Constants.ROBOT.DEAD_ZONE_MAX.get() && controller.getX(GenericHID.Hand.kLeft) >= -Constants.ROBOT.DEAD_ZONE_MIN.get() ? 0.0 : yspeedLimiter.calculate(controller.getX(GenericHID.Hand.kLeft))) * Constants.ROBOT.MAX_SPEED.get();
-    // //Rotation
-    // double rightX = controller.getX(GenericHID.Hand.kRight) <= Constants.ROBOT.DEAD_ZONE_MAX.get() && controller.getX(GenericHID.Hand.kRight) >= -Constants.ROBOT.DEAD_ZONE_MIN.get() ? 0.0 : -rotLimiter.calculate(controller.getX(GenericHID.Hand.kRight)) * Constants.ROBOT.MAX_SPEED.get();
-    double leftX;
-    double leftY;
-    double rightX;
-  
-    if(controller.getY(GenericHID.Hand.kLeft) <= Constants.ROBOT.DEAD_ZONE_MAX.get() && controller.getY(GenericHID.Hand.kLeft) >= -Constants.ROBOT.DEAD_ZONE_MIN.get()){
-      leftY = -xspeedLimiter.calculate(controller.getY(GenericHID.Hand.kLeft)) * Constants.ROBOT.MAX_SPEED.get();
-    }
-    else{
-      leftY = 0.0;
-    }
+    //Tennery used to control the deadzone
+    //Regular Movement 
+    double leftY = -xspeedLimiter.calculate(controller.getY(GenericHID.Hand.kLeft) >= Constants.ROBOT.DEAD_ZONE_MAX.get() || controller.getY(GenericHID.Hand.kLeft) <= Constants.ROBOT.DEAD_ZONE_MIN.get() ? controller.getY(GenericHID.Hand.kLeft) : 0) * 0.8;
+    double leftX = -yspeedLimiter.calculate(controller.getX(GenericHID.Hand.kLeft) >= Constants.ROBOT.DEAD_ZONE_MAX.get() || controller.getX(GenericHID.Hand.kLeft) <= Constants.ROBOT.DEAD_ZONE_MIN.get() ? controller.getX(GenericHID.Hand.kLeft) : 0) * 0.8;
+    //Rotation
+    double rightX = -rotLimiter.calculate(controller.getX(GenericHID.Hand.kRight) >= Constants.ROBOT.DEAD_ZONE_MAX.get() || controller.getX(GenericHID.Hand.kRight) <= Constants.ROBOT.DEAD_ZONE_MIN.get() ? controller.getX(GenericHID.Hand.kRight) : 0) * 0.8;
 
-    if(controller.getX(GenericHID.Hand.kLeft) <= Constants.ROBOT.DEAD_ZONE_MAX.get() && controller.getX(GenericHID.Hand.kLeft) >= -Constants.ROBOT.DEAD_ZONE_MIN.get()){
-      leftX = yspeedLimiter.calculate(controller.getX(GenericHID.Hand.kLeft)) * Constants.ROBOT.MAX_SPEED.get();
-    }
-    else{
-      leftX = 0.0;
-    }
-
-    if(controller.getX(GenericHID.Hand.kRight) <= Constants.ROBOT.DEAD_ZONE_MAX.get() && controller.getX(GenericHID.Hand.kRight) >= -Constants.ROBOT.DEAD_ZONE_MIN.get()){
-      rightX = -rotLimiter.calculate(controller.getX(GenericHID.Hand.kRight)) * Constants.ROBOT.MAX_SPEED.get();
-    }
-    else{
-      rightX = 0.0;
-    }
-
-    driveTrain.drive(leftX, leftY, rightX);
+    driveTrain.drive(leftY *  0.2, leftX * 0.2, rightX*0.2);
     //driveTrain.setMotorSpeed(3, 0.2);
     //Displays joystick values on Smart Dashboard
     SmartDashboard.putNumber("Left Y", leftY);
