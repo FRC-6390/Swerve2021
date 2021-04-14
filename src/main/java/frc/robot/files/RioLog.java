7304bee445822a79ecea78f94edbb84fba410bf7
Mathias <mathias@pepservices.com>
2021-04-14 16:08:45 -0700

    ahhhhhhhhhhhh

--- src/main/java/frc/robot/files/RioLog.java
+++ src/main/java/frc/robot/files/RioLog.java
@@ -16,7 +16,7 @@ import java.awt.*;
 import frc.robot.Constants;
 
 
-public class RioLogs {
+public class RioLog {
 
     private static File m_File, m_Destination, m_Folder, m_USBFolder;// m_subFolder, m_subUSBFolder;
     private static long m_Time;
